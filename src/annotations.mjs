a6e0dfee3df8ab31cae1ab010ead8b130fa5afdf
olemorud <ole.kristian.morud@cern.ch>
2023-06-29 09:38:05 +0200

    Replace `env:` with `with:`, reformat
    (Evil commit that also replaces all tabs with spaces and makes lines
    shorter than 80 characters)
    
    Use `with:` and `core.getInput()` instead of `process.env.[...]` to get
    user defined variables.

--- src/annotations.mjs
+++ src/annotations.mjs
@@ -3,38 +3,42 @@
 import { Octokit } from 'octokit';
 import core from '@actions/core';
 
-if (!process.env.GH_TOKEN) {
+const GH_TOKEN   = core.getInput('GH_TOKEN');
+const workflowID = core.getInput('workflow_id');
+const repoName   = process.env.GITHUB_REPOSITORY;
+const filter     = core.getInput('filter') || ""; // empty filters match
+                                                  // everything
+
+if (!GH_TOKEN) {
     console.error('Error: The GH_TOKEN environment variable is not set.');
     process.exit(1);
 }
 
-if (!process.env.workflow_id) {
-    console.error('Error: The workflow_id environment variable is not set.');
+if (!workflowID) {
+    console.error('Error: The workflowID environment variable is not set.');
     process.exit(1);
 }
 
-if (!process.env.GITHUB_REPOSITORY) {
-    console.error('Error: The GITHUB_REPOSITORY environment variable is not set.');
+if (!repoName) {
+    console.error(
+        'Error: The GITHUB_REPOSITORY environment variable is not set.');
     process.exit(1);
 }
 
-const filter = core.getInput('filter') || "";
-
-// empty filters will match everything
 const regexFilter = new RegExp(filter);
-console.log(regexFilter);
+console.log(`"${filter}" =  ${regexFilter}`);
 
 var badAnnotations = [];
 
-const octokit = new Octokit({ auth: process.env.GH_TOKEN });
+const octokit = new Octokit({ auth: GH_TOKEN });
 
 // Get the jobs in the workflow ...
 const { data: { jobs } } = await octokit.request(
     'GET /repos/{owner}/{repo}/actions/runs/{run_id}/jobs',
     {
-        owner: process.env.GITHUB_REPOSITORY.split('/')[0],
-        repo: process.env.GITHUB_REPOSITORY.split('/')[1],
-        run_id: process.env.workflow_id,
+        owner: repoName.split('/')[0],
+        repo: repoName.split('/')[1],
+        run_id: workflowID,
         mediaType: {
             format: 'json',
             previews: ['scarlet-witch']
@@ -43,7 +47,7 @@ const { data: { jobs } } = await octokit.request(
 );
 
 if ( !jobs ) {
-    console.error(`Error: could not get jobs for repository ${ process.env.GITHUB_REPOSITORY }`);
+    console.error(`Error: could not get jobs for workflow "${workflowID}"`);
 }
 
 // ... then get the annotations for each job
@@ -51,8 +55,8 @@ for (const job of jobs) {
     const { data: annotations } = await octokit.request(
         'GET /repos/{owner}/{repo}/check-runs/{check_run_id}/annotations',
         {
-            owner: process.env.GITHUB_REPOSITORY.split('/')[0],
-            repo: process.env.GITHUB_REPOSITORY.split('/')[1],
+            owner: repoName.split('/')[0],
+            repo: repoName.split('/')[1],
             check_run_id: job.id,
             mediaType: {
                 format: 'json',
@@ -63,27 +67,32 @@ for (const job of jobs) {
 
     if ( !annotations ) {
         // this is not always a bug, could just be a run with no annotations
-        console.info(`info: could not get annotations for data for run ${job.id}.`);
-		continue;
+        console.info(
+            `info: could not get annotations for data for run ${job.id}.`);
+        continue;
     }
 
     for (const an of annotations) {
-		const text = an.title + an.message;
+        const text = an.title + an.message;
 
-        if (['warning', 'failure'].includes(an.annotation_level.toLowerCase())) {
-			console.log("debug: trying to match " + text + " to " + filter);
-			if (regexFilter.test(text)) {
-				console.log(an);
-				badAnnotations.push(an);
-			}
-		}
+        if (['warning', 'failure'].includes(an.annotation_level.toLowerCase()))
+        {
+            console.log(
+                "debug: trying to match " + text + " to " + regexFilter);
+
+            if (regexFilter.test(text)) {
+                console.log(an);
+                badAnnotations.push(an);
+            }
+        }
     }
 }
 
 if (badAnnotations.length > 0) {
-	core.setFailed(
-		`Found ${ badAnnotations.length } annotations of 'warning' or higher severity.`
-	);
+    core.setFailed(
+        `Found ${ badAnnotations.length } annotations of 'warning' or higher` +
+        `severity.`
+    );
 }
 
 
